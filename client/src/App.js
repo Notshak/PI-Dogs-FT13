2d63b6804ca33cfa219f2cc8d685cf114be2b864
Notshak <imntshk@.gmail.com>
2021-06-07 12:33:32 -0300

    agrege los get del anterior PI/Dogos a este e instale axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Route } from "react-router-dom"
+import Home from "./components/Home"
+import LandingPage from './components/LandingPage';
+import CreateDog from './components/CreateDog';
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
+      <Route path='/' exact component={LandingPage}/>
+      <Route path='/home'  component={Home}/>
+      <Route path='/home/createdog'  component={CreateDog}/>
     </div>
   );
 }
---
+++ client/src/components/CreateDog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function CreateDog(){
+    return (
+        <div>
+            CreateDog
+        </div>
+    )
+}
+export default CreateDog
